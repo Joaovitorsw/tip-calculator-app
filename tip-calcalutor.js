e1567ec844acf2ed85a0c7a8a3e301f155582099
Joaovitorsw <joaovitorwbr@gmail.com>
2021-08-26 16:13:03 -0300

    ✨ feat: custom input automatic  add "%"

--- tip-calcalutor.js
+++ tip-calcalutor.js
@@ -6,3 +6,19 @@ const $buttonReset = document.querySelector(".reset");
 const $resultAmount = document.querySelector(".result-amount");
 const $resultTotal = document.querySelector(".result-total");
 const $inputs = document.querySelectorAll("input[data-form]");
+
+$buttonReset.addEventListener("click", displayReset);
+
+$customInput.addEventListener("input", () => {
+  const saveValue = (data) => {
+    $customInput.dataset.value = data;
+  };
+  const focusNumber = () => {
+    const length = $customInput.dataset.value.length;
+    $customInput.setSelectionRange(length, length);
+  };
+  const value = replaceNonNumbers($customInput);
+  saveValue(value);
+  $customInput.value = `${value}%`;
+  focusNumber();
+});
